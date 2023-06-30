f8f7718460be1e26b534964c93e25d32b7653281
gowsalyanatraj <pinkygowsi717@gmail.com>
2023-06-30 09:38:50 +0530

    add padding in sign up screen

--- lib/signup_screen.dart
+++ lib/signup_screen.dart
@@ -19,14 +19,19 @@ class _SignUpScreenState extends State<SignUpScreen> {
           children: [
             const SizedBox(height: 10.0),
             Image.asset("assets/health1.png"),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
-              child: TextField(
-                decoration: InputDecoration(
-                    labelText: "Full Name",
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.person)),
-              ),
+            Column(
+              children: [
+                const Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        labelText: "Full Name",
+                        border: OutlineInputBorder(),
+                        prefixIcon: Icon(Icons.person)),
+                  ),
+                ),
+              ],
             ),
             const Padding(
               padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
@@ -35,6 +40,15 @@ class _SignUpScreenState extends State<SignUpScreen> {
                     labelText: "Email Address",
                     border: OutlineInputBorder(),
                     prefixIcon: Icon(Icons.email)),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                    labelText: "Email Password",
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.lock)),
               ),
             ),
             const Padding(
