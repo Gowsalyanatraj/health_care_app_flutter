7d9f57a1a4e3a5389b1e26c5fb486b5b66ac751c
gowsalyanatraj <pinkygowsi717@gmail.com>
2023-07-07 09:26:24 +0530

    chore

--- lib/navbar_roots.dart
+++ lib/navbar_roots.dart
@@ -23,6 +23,27 @@ class _NavBarRootsState extends State<NavBarRoots> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: Container(
+        height: 80,
+        child: BottomNavigationBar(
+          backgroundColor: Colors.white,
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: Color(0xFF7165D6),
+          unselectedItemColor: Colors.black26,
+          selectedLabelStyle: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 15
+          ),
+          currentIndex: _selectedIndex,
+          onTap: (index) {
+            
+          },
+
+        ),
+      ),
+    );
   }
 }
