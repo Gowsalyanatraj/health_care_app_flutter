0842afed808a3327a61b9dddb759527880a9d668
gowsalyanatraj <pinkygowsi717@gmail.com>
2023-07-14 15:46:30 +0530

    feat: creating doctor profile

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,20 +1,13 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  List symptoms = [
-    "Fever",
-    "Sinus",
-    "Cough",
-    "Head-ache"
-  ];
+  List symptoms = ["Fever", "Sinus", "Cough", "Head-ache"];
   List images = [
     "doctor1.jpg",
     "doctor2.jpg",
     "doctor3.jpg",
     "doctor4.jpg",
   ];
-
- 
 
   @override
   Widget build(BuildContext context) {
@@ -23,26 +16,24 @@ class HomeScreen extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Padding(padding: EdgeInsets.symmetric(horizontal: 15),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text("Hello Keerthi",
-              style: TextStyle(
-                fontSize: 35,
-                fontWeight: FontWeight.w500
-              ),
-              ),
-              CircleAvatar(
-                radius: 25,
-                backgroundImage: AssetImage('assets/doctor1.jpg'),
-              )
-            ],
-          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Hello Keerthi",
+                  style: TextStyle(fontSize: 35, fontWeight: FontWeight.w500),
+                ),
+                CircleAvatar(
+                  radius: 25,
+                  backgroundImage: AssetImage('assets/doctor1.png'),
+                )
+              ],
+            ),
           )
         ],
       ),
     );
-    
   }
 }
--- lib/welcome_screen.dart
+++ lib/welcome_screen.dart
@@ -9,98 +9,103 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      
       body: Padding(
         padding: const EdgeInsets.only(bottom: 30),
         child: Container(
+          
           width: MediaQuery.of(context).size.width,
           height: MediaQuery.of(context).size.height,
           padding: EdgeInsets.all(10),
-          child: Column(
-            children: [
-              SizedBox(height:15),
-              Align(
-                  alignment: Alignment.centerRight,
-                  child: TextButton(
-                      onPressed: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => NavBarRoots(),
-                         ));
-                      },
-                      child: Text(
-                        'SKIP',
-                        style: TextStyle(
-                          color: Color(0xFF7165D6),
-                          fontSize: 20,
-                        ),
-                      ))),
-              SizedBox(height: 30),
-              Padding(
-                padding: EdgeInsets.all(20),
-                child: Image.asset("assets/health1.png"),
-              ),
-              SizedBox(height: 0),
-              Text(
-                'Doctors Appointment',
-                style: TextStyle(
-                    color: Color(0xFF7165D6),
-                    fontSize: 35,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 1,
-                    wordSpacing: 2),
-              ),
-              SizedBox(height: 10),
-              Text(
-                'Appoint Your Doctor',
-                style: TextStyle(
-                  color: Colors.black54,
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500,
-                  letterSpacing: 1,
+          child: SafeArea(
+            child: Column(
+              
+              children: [
+                SizedBox(height:15),
+                Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                        onPressed: () {
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => NavBarRoots(),
+                           ));
+                        },
+                        child: Text(
+                          'SKIP',
+                          style: TextStyle(
+                            color: Color(0xFF7165D6),
+                            fontSize: 20,
+                          ),
+                        ))),
+                SizedBox(height: 30),
+                Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Image.asset("assets/health1.png"),
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Material(
-                    color: Color(0xFF7165D6),
-                    borderRadius: BorderRadius.circular(10),
-                    child: InkWell(
-                      onTap: () {
-                         Navigator.push(context, MaterialPageRoute(builder: (context) => const LoginScreen(),
-                         ));
-                      },
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 15, horizontal: 40),
-                        child: Text(
-                          'Login',
-                          style: TextStyle(
-                              color: Colors.white, fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                    ),
+                SizedBox(height: 0),
+                Text(
+                  'Doctors Appointment',
+                  style: TextStyle(
+                      color: Color(0xFF7165D6),
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1,
+                      wordSpacing: 2),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Appoint Your Doctor',
+                  style: TextStyle(
+                    color: Colors.black54,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                    letterSpacing: 1,
                   ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
                     Material(
-                    color: Color(0xFF7165D6),
-                    borderRadius: BorderRadius.circular(10),
-                    child: InkWell(
-                      onTap: () {},
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 15, horizontal: 40),
-                        child: Text(
-                          'Sign Up',
-                          style: TextStyle(
-                              color: Colors.white, fontWeight: FontWeight.bold),
+                      color: Color(0xFF7165D6),
+                      borderRadius: BorderRadius.circular(10),
+                      child: InkWell(
+                        onTap: () {
+                           Navigator.push(context, MaterialPageRoute(builder: (context) => const LoginScreen(),
+                           ));
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 15, horizontal: 40),
+                          child: Text(
+                            'Login',
+                            style: TextStyle(
+                                color: Colors.white, fontWeight: FontWeight.bold),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              )
-            ],
+                      Material(
+                      color: Color(0xFF7165D6),
+                      borderRadius: BorderRadius.circular(10),
+                      child: InkWell(
+                        onTap: () {},
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 15, horizontal: 40),
+                          child: Text(
+                            'Sign Up',
+                            style: TextStyle(
+                                color: Colors.white, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
