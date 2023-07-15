60124f33daad611fc04c623c29907b09f2e7ff96
gowsalyanatraj <pinkygowsi717@gmail.com>
2023-07-15 16:09:57 +0530

    feat:adding button for the doctor appointments to create

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,7 +12,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      padding: EdgeInsets.only(top: 40),
+      padding: const EdgeInsets.only(top: 40),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -32,23 +32,37 @@ class HomeScreen extends StatelessWidget {
               ],
             ),
           ),
-          SizedBox(height: 30),
+          const SizedBox(height: 30),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               InkWell(
                 onTap: () {},
                 child: Container(
-                  padding: EdgeInsets.all(20),
+                  padding: const EdgeInsets.all(20),
                   decoration: BoxDecoration(
-                      color: Color(0xFF7165D6),
+                      color: const Color(0xFF7165D6),
                       borderRadius: BorderRadius.circular(10),
-                      boxShadow: [
+                      boxShadow: const [
                         BoxShadow(
                             color: Colors.black12,
                             blurRadius: 6,
                             spreadRadius: 4)
                       ]),
+                      child: Column(
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.all(0.8),
+                            decoration: const BoxDecoration(
+                              color: Colors.white,
+                              shape: BoxShape.circle
+                            ),
+                            child: const Icon(Icons.add,
+                            color: Color(0xFF7165D6),
+                            )
+                          )
+                        ],
+                      ),
                 ),
               )
             ],
