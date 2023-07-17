91ef13b71a4cb0e54fa0f2141122efbf8235d2f7
gowsalyanatraj <pinkygowsi717@gmail.com>
2023-07-17 16:49:56 +0530

    chore

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -76,7 +76,48 @@ class HomeScreen extends StatelessWidget {
                     ],
                   ),
                 ),
-              )
+              ),
+                 InkWell(
+                onTap: () {},
+                child: Container(
+                  padding: const EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                      color: const Color(0xFF7165D6),
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: const [
+                        BoxShadow(
+                            color: Colors.black12,
+                            blurRadius: 6,
+                            spreadRadius: 4)
+                      ]),
+                  child: Column(
+                    children: [
+                      Container(
+                          padding: const EdgeInsets.all(0.8),
+                          decoration: const BoxDecoration(
+                              color: Colors.white, shape: BoxShape.circle),
+                          child: const Icon(
+                            Icons.add,
+                            color: Color(0xFF7165D6),
+                            size: 35,
+                          )),
+                      const SizedBox(height: 30.0),
+                      const Text(
+                        "Client Visit",
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.w500),
+                      ),
+                      const SizedBox(height: 5.0),
+                      Text(
+                        "Create an Appointment",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
             ],
           )
         ],
