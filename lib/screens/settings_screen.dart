214c99f5edf20d6bfa9a196d7daab4f5bdae9dd5
gowsalyanatraj <pinkygowsi717@gmail.com>
2023-10-26 16:48:22 +0530

    chore

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -12,11 +12,28 @@ class _SettingScreenState extends State<SettingScreen> {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.only(top: 50, left: 20, right: 20),
-      child: Column(children: [
-        Text("Settings",
-        style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
-        ),
-      ],),
+      child: Column(
+        children: [
+          Text(
+            "Settings",
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
+          ),
+          SizedBox(height: 30),
+          ListTile(
+            leading: CircleAvatar(
+              radius: 30,
+              backgroundImage: AssetImage("assets/doctor2.png"),
+            ),
+            title: Text(
+              "Dr. Doctor Name",
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 25,
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
