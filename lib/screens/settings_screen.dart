bd2fde25f9435ae64e65d152db6d76d09550abaf
gowsalyanatraj <pinkygowsi717@gmail.com>
2023-10-26 16:43:43 +0530

    feat: creating settings page for mobile application

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class SettingScreen extends StatefulWidget {
   const SettingScreen({super.key});
 
@@ -9,6 +10,13 @@ class SettingScreen extends StatefulWidget {
 class _SettingScreenState extends State<SettingScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Padding(
+      padding: EdgeInsets.only(top: 50, left: 20, right: 20),
+      child: Column(children: [
+        Text("Settings",
+        style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
+        ),
+      ],),
+    );
   }
 }
