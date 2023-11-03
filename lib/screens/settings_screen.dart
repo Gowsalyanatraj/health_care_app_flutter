e7a0be5d82fb724e4471b1ae9dafc6a27610fc54
gowsalyanatraj <pinkygowsi717@gmail.com>
2023-11-03 10:49:41 +0530

    feat: creating profile, notification, privacy general and about us settings. || style: designing icons and text alignment for settings

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -61,37 +61,100 @@ class _SettingScreenState extends State<SettingScreen> {
           const SizedBox(
             height: 20.0,
           ),
-          const ListTile(
-            leading: CircleAvatar(
-              radius: 30,
-              backgroundImage: AssetImage("assets/doctor2.png"),
-            ),
-            title: Text(
-              "Dr. Doctor Name",
-              style: TextStyle(
-                fontWeight: FontWeight.w500,
-                fontSize: 25,
-              ),
-            ),
-            subtitle: Text("Profile"),
-          ),
-          const Divider(height: 50),
           ListTile(
             onTap: () {},
             leading: Container(
               padding: const EdgeInsets.all(10),
               decoration: BoxDecoration(
-                color: Colors.blue.shade100,
+                color: Colors.indigo.shade100,
                 shape: BoxShape.circle,
               ),
               child: const Icon(
                 Icons.notifications_none_outlined,
-                color: Colors.blue,
+                color: Colors.deepPurple,
                 size: 35,
               ),
             ),
             title: const Text(
               "Notification",
+              style: TextStyle(
+                fontWeight: FontWeight.w400,
+                fontSize: 20.0,
+              ),
+            ),
+            trailing: const Icon(Icons.arrow_forward_ios_rounded),
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Container(
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.indigo.shade100,
+                shape: BoxShape.circle,
+              ),
+              child: const Icon(
+                Icons.privacy_tip_outlined,
+                color: Colors.indigo,
+                size: 35,
+              ),
+            ),
+            title: const Text(
+              "Privacy",
+              style: TextStyle(
+                fontWeight: FontWeight.w400,
+                fontSize: 20.0,
+              ),
+            ),
+            trailing: const Icon(Icons.arrow_forward_ios_rounded),
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Container(
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.green.shade100,
+                shape: BoxShape.circle,
+              ),
+              child: const Icon(
+                Icons.settings_suggest_outlined,
+                color: Colors.green,
+                size: 35,
+              ),
+            ),
+            title: const Text(
+              "General",
+              style: TextStyle(
+                fontWeight: FontWeight.w400,
+                fontSize: 20.0,
+              ),
+            ),
+            trailing: const Icon(Icons.arrow_forward_ios_rounded),
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Container(
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.teal.shade100,
+                shape: BoxShape.circle,
+              ),
+              child: const Icon(
+                Icons.info_outline,
+                color: Colors.teal,
+                size: 35,
+              ),
+            ),
+            title: const Text(
+              "About us",
               style: TextStyle(
                 fontWeight: FontWeight.w400,
                 fontSize: 20.0,
