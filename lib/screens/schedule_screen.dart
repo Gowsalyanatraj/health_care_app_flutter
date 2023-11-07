10336079b6a810035b88f1ea137149118570296b
gowsalyanatraj <pinkygowsi717@gmail.com>
2023-11-07 14:57:21 +0530

    feat: create schedule settings and configration

---
+++ lib/screens/schedule_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ScheduleScreen extends StatefulWidget {
+  const ScheduleScreen({super.key});
+
+  @override
+  State<ScheduleScreen> createState() => _ScheduleScreenState();
+}
+
+class _ScheduleScreenState extends State<ScheduleScreen> {
+  int _buttonIndex = 0;
+  final _scheduleWidgets = [
+    // UpcomingSchedule(),
+    Container(),
+    //Upcoming Schedule(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
