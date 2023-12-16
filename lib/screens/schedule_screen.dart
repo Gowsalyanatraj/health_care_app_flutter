bee0ced6ff273fedf730aaf1a41d311d73c58534
gowsalyanatraj <pinkygowsi717@gmail.com>
2023-12-16 17:14:49 +0530

    chore

--- lib/screens/schedule_screen.dart
+++ lib/screens/schedule_screen.dart
@@ -1,128 +1,128 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// class ScheduleScreen extends StatefulWidget {
-//   const ScheduleScreen({super.key});
+class ScheduleScreen extends StatefulWidget {
+  const ScheduleScreen({super.key});
 
-//   @override
-//   State<ScheduleScreen> createState() => _ScheduleScreenState();
-// }
+  @override
+  State<ScheduleScreen> createState() => _ScheduleScreenState();
+}
 
-// class _ScheduleScreenState extends State<ScheduleScreen> {
-//   int _buttonIndex = 0;
-//   final _scheduleWidgets = [
-//     // UpcomingSchedule(),
-//     const Center(child: Text("Upcoming")),
-//     const Center(child: Text("Completed")),
-//     const Center(child: Text("Cancelled")),
-//   ];
-//   @override
-//   Widget build(BuildContext context) {
-//     return SingleChildScrollView(
-//       child: Padding(
-//         padding: const EdgeInsets.only(top: 40),
-//         child: Column(
-//           crossAxisAlignment: CrossAxisAlignment.start,
-//           children: [
-//             const Padding(
-//               padding: EdgeInsets.symmetric(horizontal: 15),
-//               child: Text(
-//                 "Schedule",
-//                 style: TextStyle(
-//                   fontSize: 32,
-//                   fontWeight: FontWeight.w500,
-//                 ),
-//               ),
-//             ),
-//             const SizedBox(height: 20),
-//             Container(
-//               padding: const EdgeInsets.all(5),
-//               margin: const EdgeInsets.symmetric(horizontal: 10),
-//               decoration: BoxDecoration(
-//                   color: const Color(0xfff4f6fa),
-//                   borderRadius: BorderRadius.circular(10)),
-//               child: Row(
-//                 mainAxisAlignment: MainAxisAlignment.center,
-//                 children: [
-//                   InkWell(
-//                     onTap: () {
-//                       setState(() {
-//                         _buttonIndex = 0;
-//                       });
-//                     },
-//                     child: Container(
-//                       padding: const EdgeInsets.symmetric(
-//                           vertical: 12, horizontal: 15),
-//                       decoration: BoxDecoration(
-//                         color: _buttonIndex == 0
-//                             ? const Color(0xff7165d6)
-//                             : Colors.transparent,
-//                         borderRadius: BorderRadius.circular(10),
-//                       ),
-//                       child: const Text(
-//                         "Upcoming",
-//                         style: TextStyle(
-//                           fontSize: 16,
-//                           fontWeight: FontWeight.w500,
-//                         ),
-//                       ),
-//                     ),
-//                   ),
-//                   InkWell(
-//                     onTap: () {
-//                       setState(() {
-//                         _buttonIndex = 1;
-//                       });
-//                     },
-//                     child: Container(
-//                       padding: const EdgeInsets.symmetric(
-//                           vertical: 12, horizontal: 25),
-//                       decoration: BoxDecoration(
-//                         color: _buttonIndex == 1
-//                             ? const Color(0xff7165d6)
-//                             : Colors.transparent,
-//                         borderRadius: BorderRadius.circular(10),
-//                       ),
-//                       child: const Text(
-//                         "Completed",
-//                         style: TextStyle(
-//                           fontSize: 16,
-//                           fontWeight: FontWeight.w500,
-//                         ),
-//                       ),
-//                     ),
-//                   ),
-//                   InkWell(
-//                     onTap: () {
-//                       setState(() {
-//                         _buttonIndex = 2;
-//                       });
-//                     },
-//                     child: Container(
-//                       padding: const EdgeInsets.symmetric(
-//                           vertical: 12, horizontal: 25),
-//                       decoration: BoxDecoration(
-//                         color: _buttonIndex == 2
-//                             ? const Color(0xff7165d6)
-//                             : Colors.transparent,
-//                         borderRadius: BorderRadius.circular(10),
-//                       ),
-//                       child: const Text(
-//                         "Cancelled",
-//                         style: TextStyle(
-//                           fontSize: 16,
-//                           fontWeight: FontWeight.w500,
-//                         ),
-//                       ),
-//                     ),
-//                   )
-//                 ],
-//               ),
-//             ),
-//              const SizedBox(height: 30.0),
-//             _scheduleWidgets[_buttonIndex],
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+class _ScheduleScreenState extends State<ScheduleScreen> {
+  int _buttonIndex = 0;
+  final _scheduleWidgets = [
+    // UpcomingSchedule(),
+    const Center(child: Text("Upcoming")),
+    const Center(child: Text("Completed")),
+    const Center(child: Text("Cancelled")),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 40),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: Text(
+                "Schedule",
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              padding: const EdgeInsets.all(5),
+              margin: const EdgeInsets.symmetric(horizontal: 10),
+              decoration: BoxDecoration(
+                  color: const Color(0xfff4f6fa),
+                  borderRadius: BorderRadius.circular(10)),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  InkWell(
+                    onTap: () {
+                      setState(() {
+                        _buttonIndex = 0;
+                      });
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 12, horizontal: 15),
+                      decoration: BoxDecoration(
+                        color: _buttonIndex == 0
+                            ? const Color(0xff7165d6)
+                            : Colors.transparent,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: const Text(
+                        "Upcoming",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      setState(() {
+                        _buttonIndex = 1;
+                      });
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 12, horizontal: 25),
+                      decoration: BoxDecoration(
+                        color: _buttonIndex == 1
+                            ? const Color(0xff7165d6)
+                            : Colors.transparent,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: const Text(
+                        "Completed",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      setState(() {
+                        _buttonIndex = 2;
+                      });
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 12, horizontal: 25),
+                      decoration: BoxDecoration(
+                        color: _buttonIndex == 2
+                            ? const Color(0xff7165d6)
+                            : Colors.transparent,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: const Text(
+                        "Cancelled",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+             const SizedBox(height: 30.0),
+            _scheduleWidgets[_buttonIndex],
+          ],
+        ),
+      ),
+    );
+  }
+}
