4bf04cf65f6bc3fff4fdc0933ed56e6096b9a79d
gowsalyanatraj <pinkygowsi717@gmail.com>
2024-01-30 16:51:53 +0530

    chore

--- lib/widgets/upcoming_schedule.dart
+++ lib/widgets/upcoming_schedule.dart
@@ -1,46 +1,46 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class UpComingSchedule extends StatelessWidget {
-  const UpComingSchedule({super.key});
+// class UpComingSchedule extends StatelessWidget {
+//   const UpComingSchedule({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 15),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text("About"),
-          SizedBox(height: 15),
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 0.5),
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [
-                  BoxShadow(
-                      color: Colors.black12, blurRadius: 4, spreadRadius: 2)
-                ]),
-            child: SizedBox(
-              width: MediaQuery.of(context).size.width,
-              child: Column(
-                children: [
-                  ListTile(
-                    title: Text(
-                      "Dr. Doctor Name",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    subtitle: Text("Therapist"),
-                    trailing: CircleAvatar(
-                      radius: 25,
-                    ),
-                  )
-                ],
-              ),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//       padding: EdgeInsets.symmetric(horizontal: 15),
+//       child: Column(
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//         children: [
+//           Text("About"),
+//           SizedBox(height: 15),
+//           Container(
+//             padding: EdgeInsets.symmetric(vertical: 0.5),
+//             decoration: BoxDecoration(
+//                 color: Colors.white,
+//                 borderRadius: BorderRadius.circular(10),
+//                 boxShadow: [
+//                   BoxShadow(
+//                       color: Colors.black12, blurRadius: 4, spreadRadius: 2)
+//                 ]),
+//             child: SizedBox(
+//               width: MediaQuery.of(context).size.width,
+//               child: Column(
+//                 children: [
+//                   ListTile(
+//                     title: Text(
+//                       "Dr. Doctor Name",
+//                       style: TextStyle(fontWeight: FontWeight.bold),
+//                     ),
+//                     subtitle: Text("Therapist"),
+//                     trailing: CircleAvatar(
+//                       radius: 25,
+//                     ),
+//                   )
+//                 ],
+//               ),
+//             ),
+//           )
+//         ],
+//       ),
+//     );
+//   }
+// }
